870ba93d0ab7f647f3d6758f229661f27715d98e
naitikkkkkk <98087490+naitikkkkkk@users.noreply.github.com>
2022-03-11 16:31:13 +0530

    Awesome Quiz

---
+++ js/questions.js
@@ -0,0 +1,143 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Where Is Indra Gandhi Center For Atomic Research Located?",
+    answer: "Tamil Nadu,Kalpakkam",
+    options: [
+      "Tamil Nadu,Kalpakkam",
+      "Rajasthan,Ajmer",
+      "West Bengal,Kolkata",
+      "Punjab,Chandigrah"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Full Form Of Amul",
+    answer: "Anand Milk Union Limited",
+    options: [
+      "America Milk Union Limited",
+      "Aman Milk United Limited",
+      "Anand Milk Union Limited",
+      "Anil Milk Union Limted"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Who Was The First Women Judge Of The High Court?",
+    answer: "Fatima Biwi",
+    options: [
+    "Fatima Biwi",
+      "Gyan Sugha Misra",
+      "Sujata Manohar",
+      "Ruma Pal"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who Invented Python?",
+    answer: "Guido Van Rossum",
+    options: [
+      "Håkon Wium Lie",
+      "Tim Berners-Lee",
+      "Guido Van Rossum",
+      "Lord Lytton"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Where Is The Headquarters Of UNESCO Located?",
+    answer: "Paris",
+    options: [
+      "Paris",
+      "Tokyo",
+      "California",
+      "Germany"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Who invented HTML?",
+    answer: "Tim Berners Lee",
+    options: [
+      "Tim Berners Lee",
+      "Lord Canning",
+      "Wium Lie",
+      "Guido Van Rossum"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which Party Was Formd Of India By A.U Hume?",
+    answer: "India National Congress",
+    options: [
+      "India National Congress",
+      "Bhartiya Janta Party",
+      "Communist Party OF India",
+      "Communist Party Of India (Marxist)"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Near Which River Statue Of Unity Located?",
+    answer: "Narmada River",
+    options: [
+      "Sabarmati River",
+      "Godavri River",
+      "Kaveri River",
+      "Narmada River"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "Where Is Silicon Valley Located?",
+    answer: "California,U.S",
+    options: [
+      "Heroshima,Japan",
+      "Bengaluru,Karnataka",
+      "California,U.S",
+      "Bejing,China"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "In Which Year Was The Reserve Bank Of India Established?",
+    answer: "1935",
+    options: [
+      "1949",
+      "1939",
+      "1940",
+      "1935"
+    ]
+  },
+
+
+  {
+    numb: 9,
+    question: "When was Shri Lata Mangeshkar Died?",
+    answer: "6 Feb 2022",
+    options: [
+      "2 Feb 2022",
+      "6 Feb 2022",
+      "10 Feb 2022",
+      "9 Feb 2022"
+    ]
+  },
+
+
+  {
+    numb: 10,
+    question: "1024 Kilobytes is equal to?",
+    answer: "1 Megabyte (MB)",
+    options: [
+      "0.5 Megabyte (kb)",
+      "1 Megabyte (MB)",
+      "5 Megabyte (MB)",
+      "1.5 Megabyte (MB)"
+    ]
+  },
+
+];
